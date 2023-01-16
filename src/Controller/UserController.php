03c1a6dea41bfc936e0fe3d729e9f13adef45d86
Filip Horvat <fico7489@gmail.com>
2023-01-16 16:02:23 +0100

    user update initially

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\ContactForm;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -30,13 +32,29 @@ class UserController extends AbstractController
         ]);
     }
 
-    #[Route('/users/{id}/edit', name: 'users.edit', methods: ['GET'])]
-    public function edit(EntityManagerInterface $em, int $id): Response
+    #[Route('/users/{id}/edit', name: 'users.edit', methods: ['GET', 'PATCH'])]
+    public function edit(EntityManagerInterface $em, Request $request, int $id): Response
     {
         $user = $em->getRepository(User::class)->find($id);
 
+        $form = $this->createForm(ContactForm::class, $user);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getData();
+
+            if ($user instanceof User) {
+                $em->persist($user);
+                $em->flush();
+            }
+
+            $this->addFlash('success', 'Contact successfully updated!');
+
+            return $this->redirectToRoute('users.index');
+        }
+
         return $this->render('users/edit.html.twig', [
-            'user' => $user,
+            'form' => $form,
         ]);
     }
 
