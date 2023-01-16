597f2486947ce0f05c0a3fae6bcdaa3aa754bacb
Filip Horvat <fico7489@gmail.com>
2023-01-16 14:07:26 +0100

    show user initial

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -10,13 +10,23 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {
-    #[Route('/users', name: 'users.index')]
+    #[Route('/users', name: 'users.index', methods: ['GET'])]
     public function index(EntityManagerInterface $em): Response
     {
         $users = $em->getRepository(User::class)->findAll();
 
         return $this->render('users/index.html.twig', [
             'users' => $users,
+        ]);
+    }
+
+    #[Route('/users/{id}', name: 'users.show', methods: ['GET'])]
+    public function show(EntityManagerInterface $em, int $id): Response
+    {
+        $user = $em->getRepository(User::class)->find($id);
+
+        return $this->render('users/show.html.twig', [
+            'user' => $user,
         ]);
     }
 }
