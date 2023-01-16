7d59c71e87aeba86e127318904fe0be940274902
Filip Horvat <fico7489@gmail.com>
2023-01-16 12:32:28 +0100

    add twig to project

---
+++ src/Controller/UserController.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    #[Route('/users', name: 'users.index')]
+    public function list(): Response
+    {
+        $users = [];
+
+        return $this->render('users/index.html.twig', [
+            'users' => $users,
+        ]);
+    }
+}
