457fc2011341f6c12e839da4a2dee5fda3ffa6ec
Filip Horvat <fico7489@gmail.com>
2023-01-16 16:14:58 +0100

    add symfony validator, add validation for email

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 class User
@@ -20,6 +21,7 @@ class User
     private ?string $last_name = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\Email(message: 'The email {{ value }} is not a valid email.', )]
     private ?string $email = null;
 
     #[ORM\Column]
