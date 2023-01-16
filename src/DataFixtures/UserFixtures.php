6217361c79b4381f71db14a7234cb462f384ce43
Filip Horvat <fico7489@gmail.com>
2023-01-16 13:18:50 +0100

    add user fixtures

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class UserFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        // create 20 products! Bam!
+        for ($i = 0; $i < 20; $i++) {
+            $user = new User();
+            $user->setFirstName('first_name-'.$i);
+            $user->setLastName('last_name-'.$i);
+            $user->setEmail('first_name-'.$i . '@revendo.com');
+            $user->setFavorite(rand(0, 1));
+            $manager->persist($user);
+        }
+
+        $manager->flush();
+    }
+}
