9c4e229a663d731e5f985cf81d7fadd16a682fe9
Filip Horvat <fico7489@gmail.com>
2023-01-16 13:12:26 +0100

    initial show users

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,9 +11,9 @@ use Symfony\Component\Routing\Annotation\Route;
 class UserController extends AbstractController
 {
     #[Route('/users', name: 'users.index')]
-    public function list(): Response
+    public function index(EntityManagerInterface $em): Response
     {
-        $users = [];
+        $users = $em->getRepository(User::class)->findAll();
 
         return $this->render('users/index.html.twig', [
             'users' => $users,
