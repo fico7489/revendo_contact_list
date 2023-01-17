a233eda2af9ef9b5f83656c54158b6decab8389d
Filip Horvat <fico7489@gmail.com>
2023-01-17 08:01:41 +0100

    send initial email

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -8,6 +8,8 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
--- src/EventListener/EmailForCreatedUserSender.php
+++ src/EventListener/EmailForCreatedUserSender.php
@@ -4,16 +4,35 @@ namespace App\EventListener;
 
 use App\Entity\User;
 use Doctrine\ORM\Event\OnFlushEventArgs;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 
 class EmailForCreatedUserSender
 {
+    private MailerInterface $mailer;
+
+    public function __construct(MailerInterface $mailer)
+    {
+        $this->mailer = $mailer;
+    }
+
     public function onFlush(OnFlushEventArgs $eventArgs): void
     {
         $ouw = $eventArgs->getObjectManager()->getUnitOfWork();
 
         foreach ($ouw->getScheduledEntityInsertions() as $entity) {
             if ($entity instanceof User) {
-                // TODO send email
+                $email = (new TemplatedEmail())
+                    ->from('hello@example.com')
+                    ->to('you@example.com')
+                    ->subject('Time for Symfony Mailer!')
+                    ->htmlTemplate('emails/contact_created.html.twig')
+                    ->context([
+                        'user' => $entity,
+                    ]);
+
+                $this->mailer->send($email);
             }
         }
     }
